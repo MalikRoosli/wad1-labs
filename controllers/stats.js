854880c49b8d1b1b4c499fa9d63da50388651244
Malik Roosli <20117610@setu.ie>
2026-03-26 14:49:15 +0000

    App statistics added

---
+++ controllers/stats.js
@@ -0,0 +1,53 @@
+"use strict";
+import logger from "../utils/logger.js";
+import playlistStore from "../models/playlist-store.js";
+
+const stats = {
+  createView(request, response) {
+    logger.info("Stats page loading!");
+    // app statistics calculations
+    const playlists = playlistStore.getAllPlaylists();
+
+    let numPlaylists = playlists.length;
+    
+    let numSongs = playlists.reduce((total, playlist) => total + playlist.songs.length, 0);
+	
+	  let average = numPlaylists > 0 ? (numSongs / numPlaylists).toFixed(2) : 0;
+
+
+      let totalRating = playlists.reduce((total, playlist) => total + parseInt(playlist.rating), 0);
+      let avgRating = numPlaylists > 0 ? totalRating/numPlaylists : 0;
+      let mapped = playlists.map(playlist => playlist.rating);
+      let maxRating = Math.max(...mapped);
+      let maxRated = playlists.filter(playlist => playlist.rating === maxRating);
+      let favTitles = maxRated.map(item => item.title);
+      let songCounts = playlists.map(playlist => playlist.songs.length);
+      let maxSongs = songCounts.length > 0 ? Math.max(...songCounts) : 0;
+
+      let largestPlaylists = playlists.filter(
+      playlist => playlist.songs.length === maxSongs
+      );
+      let largestTitles = largestPlaylists.map(p => p.title);
+
+
+      const statistics = {
+      displayNumPlaylists: numPlaylists,
+      displayNumSongs: numSongs,
+      displayAverage: average,
+      displayAvgRating: avgRating.toFixed(2),
+      highest: maxRating,
+      displayFav: favTitles,
+      displayMaxSongs: maxSongs,
+      displayLargestPlaylists: largestTitles
+      };
+
+    const viewData = {
+      title: "Playlist App Statistics",
+      stats: statistics
+    };
+  
+    response.render("stats", viewData);
+  },
+};
+
+export default stats;
